7afa9bba227d7fcfb6cfe45f48d1d44a572b0a50
charlielangmuir <charlie.langmuir@gmail.com>
2025-01-10 10:27:30 -0500

    i <3 git

---
+++ labs/wk1/lab1.py
@@ -0,0 +1,18 @@
+#Sample Coding Questions Week01
+
+arr = [1,4,7,9];
+
+a =1;
+b=2;
+c=3;
+d=4;
+e=(a-b)(c/d) + (a%c);
+
+temperature = 32.6;
+print("the temp today is " + round(temperature , 3));
+
+def ageFunc(){
+age = input();
+age +=22;
+print("Now showing the shop items filtered by age: " + age);
+}
