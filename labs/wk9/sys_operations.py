56b2920289d31b13dbf6e43889b2dba1d81e60f3
charlielangmuir <charlie.langmuir@gmail.com>
2025-03-07 09:24:46 -0500

    Completed sys_operations.py

--- labs/wk9/sys_operations.py
+++ labs/wk9/sys_operations.py
@@ -1,6 +1,7 @@
 import os
 import platform
 import socket
+import multiprocessing as mp
 print("Current machinw type")
 print(platform.machine())
 print("====================================")
@@ -33,3 +34,30 @@ file_object_TextIO = os.fdopen(file_handle, "w+") # Open the file handle in writ
 
 file_object_TextIO.write("Hello World\n")
 file_object_TextIO.flush()
+
+
+print(f"\n[Before Process {os.getpid()} ] Forking now")
+#def fork_process():
+#os.fork()
+if __name__ == '__main__':
+    context = mp.get_context('spawn')
+    pid = context.Process()
+    pid.start()
+    pid.join()
+else: 
+    pid = os.getpid()
+if pid==0:
+    #Child process
+    print(f"\nParent process id: {os.getppid()}")
+    print(f"\nChild process id: {os.getpid()}")
+    os.lseek(file_handle, 0, 0) # Move the file pointer to the beginning of the file
+    print(f"[Child Process {os.getpid()}] FIle Content: {os.read(file_handle,100).decode()}")
+    os.close(file_handle)
+else:
+    #Parent process
+    print(f"\nParent process id: {os.getpid()}")
+#if __name__ == '__main__':
+ #   context = mp.get_context('spawn')
+  #  pid = context.Process(target=fork_process, args=())
+   # pid.start()
+    #pid.join()
