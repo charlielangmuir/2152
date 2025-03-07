827171561bf8796bf611edb5ad157ff4b6f190c4
charlielangmuir <charlie.langmuir@gmail.com>
2025-03-07 08:52:29 -0500

    added basic changes to sys_operations.py

--- labs/wk9/sys_operations.py
+++ labs/wk9/sys_operations.py
@@ -1,3 +1,24 @@
-import sys
+import os
 import platform
+import socket
+print("Current machinw type")
 print(platform.machine())
+print("====================================")
+
+print("Current processor")
+print(platform.architecture())
+print("====================================")
+
+print("Set socket timeout to 50 seconds and then retrieve it")
+print(socket.setdefaulttimeout(50))
+print(socket.getdefaulttimeout())
+print("====================================")
+
+print("Get the current operating system type")
+print(os.name)
+print("Get the current operating system name")
+print(platform.system())
+print("====================================")
+
+print("Get the current process id")
+print(os.getpid())
